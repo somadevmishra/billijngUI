09061603330b3e9974e4315db6b107b4d82d0bfc
Somadev <somadev.mishra.20four7va@gmail.com>
2026-07-14 23:42:12 +0530

    #4| initial setup for the project

---
+++ src/main/kotlin/com/billing/ui/controller/DashboardController.kt
@@ -0,0 +1,15 @@
+package com.billing.ui.controller
+
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class DashboardController {
+
+    @GetMapping("/")
+    fun home(): String = "dashboard/index"
+
+    @GetMapping("/dashboard")
+    fun dashboard(): String = "dashboard/index"
+}
